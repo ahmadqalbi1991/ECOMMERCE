8885566e64c2b549dc94c5ba32beae0b26d75953
Ahmed Qalbi <72644716+ahmadqalbi1991@users.noreply.github.com>
2025-01-21 09:23:06 -0800

    Update SiteController.php

--- app/Http/Controllers/API/SiteController.php
+++ app/Http/Controllers/API/SiteController.php
@@ -25,7 +25,7 @@ class SiteController extends Controller
 //                ->where(['parent_id' => null, 'is_active' => 1])->get();
             $banners = Banner::where(['home_page_banner' => 0, 'show_on_home' => 1])->get();
             $homeDeals = Banner::where(['home_page_banner' => 1])->limit(3)->get();
-            $categories = Category::where(['is_active' => 1, 'level' => 1])->get();
+            $categories = Category::where(['is_active' => 1, 'level' => 1])->notNull('image')->latest()->limit(12)->get();
             $discounted_products = Product::select('product_title', 'price', 'slug', 'id', 'discount_type', 'discount_value', 'unit_id', 'unit_value', 'default_image', 'apply_discount')
                 ->with(['unit' => function ($q) {
                     return $q->select('unit', 'prefix', 'id', 'id');
